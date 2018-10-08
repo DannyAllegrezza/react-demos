4690f0394381ac80993a00f760e4de4e03a141a5
DannyAllegrezza <danny@dannyallegrezza.com>
2018-10-08 16:14:48 -0400

    component updates

---
+++ demo-apps/react-redux-router-es6/src/components/common/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import LoadingDots from './LoadingDots';
+
+const Header = ({ loading }) => {
+	const activeStyle = { color: 'orange' };
+	return (
+		<nav>
+			<NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
+			{" | "}
+			<NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+			{" | "}
+			<NavLink to="/events" activeStyle={activeStyle}>Events</NavLink>
+			{loading && <LoadingDots interval={100} dots={20} />}
+		</nav>
+	);
+};
+
+Header.propTypes = {
+	loading: PropTypes.bool.isRequired
+};
+
+export default Header;
--- demo-apps/react-redux-router-es6/src/index.js
+++ demo-apps/react-redux-router-es6/src/index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import './styles/styles.css'; //Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
+
+
+render(
+		<Router>
+			<Route path="/" component={App} />
+		</Router>,
+	document.getElementById('app')
+);
